864d556c4b49fb355417aba3ed9a9f82afa29529
tunny17 <tvnji17@gmail.com>
2022-10-24 13:34:00 +0100

    monday

---
+++ sandbox.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      type: '.bullets',
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+      hiddenClass: true,
+    },
+  
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+      hide: true,
+    },
+
+    autoplay: {
+        delay: 1000,
+    },
+});
